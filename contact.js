021c2603f773a2f2ba2f171042c86960b98fc876
peterliang000 <peter.liang000@gmail.com>
2018-10-14 23:17:13 -0400

    5th update, modified about me page and added js to contact form

---
+++ contact.js
@@ -0,0 +1,5 @@
+send.onclick = function(){
+	var name = document.getElementById("contactName")
+	alert("Thank you " + contactName.value + " for contacting us! We will get back to you in 24 hours. ")
+}
+
